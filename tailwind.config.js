e1c314e91a336687aecca952f146316a4691478d
modev <modev.404@gmail.com>
2024-08-28 09:44:53 +0300

    clsx install & set up css color variables & font family & extend to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-grayish-blue": "var(--Light-grayish-blue)",
+        "very-dark-blue": "var(--Very-dark-blue)",
+        "dark-grayish-blue": "var(--Dark-grayish-blue)",
+        "grayish-blue": "var(--Grayish-blue)",
+        "white-color": "var(--White-color)",
+        "primary-orange": "var(--Orange)",
+        "primary-pale-orange": "var(--Pale-orange)",
+      },
+      fontFamily: {
+        sans: ["Kumbh Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
