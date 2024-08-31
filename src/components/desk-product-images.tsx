dd3217e8636d6b94926df393c89674d5d63e1b9c
modev <modev.404@gmail.com>
2024-08-31 14:48:19 +0300

    display desktop images

---
+++ src/components/desk-product-images.tsx
@@ -0,0 +1,45 @@
+import clsx from "clsx";
+import { useState } from "react";
+
+type Image = {
+  key: string;
+  thumbnail: string;
+  image: string;
+};
+
+type Props = {
+  images: Image[];
+};
+const DeskImages = ({ images }: Props) => {
+  const [selected, setSelected] = useState<Image>(images[0]);
+  return (
+    <div className="flex flex-col items-center p-4 space-y-6">
+      <img
+        src={selected?.image}
+        alt="product image"
+        className="object-contain rounded-lg shadow-sm"
+        style={{ maxWidth: "100%", height: "auto", maxHeight: "400px" }}
+      />
+      <div className="flex flex-row items-center justify-between space-x-3">
+        {images.map((image) => (
+          <img
+            key={image.key}
+            className={clsx(
+              "rounded-md cursor-pointer transition-transform transform hover:scale-105",
+              {
+                "border-2 border-primary-orange opacity-50":
+                  selected.key === image.key,
+              }
+            )}
+            onClick={() => setSelected(image)}
+            src={image.thumbnail}
+            alt="product thumbnail image"
+            style={{ width: "65px", height: "65px", objectFit: "cover" }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DeskImages;
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -10,4 +10,44 @@ const routes: route[] = [
   { path: "/contact", name: "Contact" },
 ];
 
-export { routes };
+type Product = {
+  name: string;
+  price: number;
+  discountRate: number;
+  description: string;
+  images: {
+    key: string;
+    thumbnail: string;
+    image: string;
+  }[];
+};
+const product: Product = {
+  name: "Fall Limited Edition Sneakers",
+  description:
+    "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they&rsquo;ll withstand everything the weather can offer.",
+  price: 250.0,
+  discountRate: 0.5,
+  images: [
+    {
+      key:"1",
+      thumbnail: "./images/image-product-1-thumbnail.jpg",
+      image: "./images/image-product-1.jpg",
+    },
+    {
+      key:"2",
+      thumbnail: "./images/image-product-2-thumbnail.jpg",
+      image: "./images/image-product-2.jpg",
+    },
+    {
+      key:"3",
+      thumbnail: "./images/image-product-3-thumbnail.jpg",
+      image: "./images/image-product-3.jpg",
+    },
+    {
+      key:"4",
+      thumbnail: "./images/image-product-4-thumbnail.jpg",
+      image: "./images/image-product-4.jpg",
+    },
+  ],
+};
+export { routes, product };
